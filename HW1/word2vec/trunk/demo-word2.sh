afff2b97316a83fa9f31917087539edc7250b2b4
Brandon Edmunds <brandonedmunds2@gmail.com>
2023-02-09 09:34:00 -0500

    .

--- HW1/word2vec/trunk/demo-word2.sh
+++ HW1/word2vec/trunk/demo-word2.sh
@@ -3,6 +3,8 @@
 # CSE 582
 # 2/15/23
 
+# res.tst is a combination of all the given files to predict top 10 words
+
 make
 time ./word2vec -train res.txt -output vectors.bin -cbow 1 -size 100 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 5
 ./distance vectors.bin
